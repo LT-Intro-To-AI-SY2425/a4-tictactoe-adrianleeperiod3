92576f2932a28c882faf4dbd7ab2eb7348b124bc
Adrian Lee <ajlee4@cps.edu>
2024-10-30 10:46:32 -0500

    started woring with Dog class

--- notes.py
+++ notes.py
@@ -2,3 +2,19 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "?", a = 0):
+        self.name = n
+        self.age = a
+
+    def __str__(self):
+        s = f"{self.name} is {self.age} years old."
+        return s
+
+logan = Dog("Logan", 8)
+airbud = Dog("Airbud", 24)
+roger = Dog()
+
+print(logan)
+print(airbud)
+print(roger)
